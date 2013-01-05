f3be2f68136db51f72c3f513ffc74e7962a72cf5
Antonis Kalou <kalouantonis@gmail.com>
2013-01-05 21:22:35 +0200

    Fixed leveling bug

--- space_fighter.cpp
+++ space_fighter.cpp
@@ -532,8 +532,8 @@ void check_score(spaceship &ship, comets comet[], life_comet &lComet)
 		{
 			comet[i].speed += 1;
 			lComet.speed += 1;
-			ship.level++;
 		}
+		ship.level++;
 		score_level *= 2;
 	}
 }
